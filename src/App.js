1c9227cbddeab14058a931b94ad96c89de84a989
HamzaAnwar1998 <anwarhamza919@gmail.com>
2021-10-25 10:19:55 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,48 @@
 import {useState} from 'react'
-import * as XLSX from 'xlsx'
 import {Data} from './Components/Data'
+import * as XLSX from 'xlsx'
 
-function App() {  
+function App() {
   
-  // onChange
+  // on change states
   const [excelFile, setExcelFile]=useState(null);
-  const [excelFileError, setExcelFileError]=useState(null);
+  const [excelFileError, setExcelFileError]=useState(null);  
+ 
   // submit
   const [excelData, setExcelData]=useState(null);
+  // it will contain array of objects
 
-  // handle excel file
-  const fileType=['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.ms-excel'];
-  const handleFile=(e)=>{
-    let selectedFile=e.target.files[0];
+  // handle File
+  const fileType=['application/vnd.ms-excel'];
+  const handleFile = (e)=>{
+    let selectedFile = e.target.files[0];
     if(selectedFile){
       // console.log(selectedFile.type);
       if(selectedFile&&fileType.includes(selectedFile.type)){
-        let reader = new FileReader();        
+        let reader = new FileReader();
         reader.readAsArrayBuffer(selectedFile);
         reader.onload=(e)=>{
           setExcelFileError(null);
           setExcelFile(e.target.result);
-        }
+        } 
       }
       else{
-          setExcelFile(null);
-          setExcelFileError('please select only excel file');
-      }               
+        setExcelFileError('Please select only excel file types');
+        setExcelFile(null);
+      }
     }
     else{
-      console.log('select your file');
+      console.log('plz select your file');
     }
   }
 
-  // consoled
-  // console.log(excelFile);
-
-  // file submit
+  // submit function
   const handleSubmit=(e)=>{
     e.preventDefault();
-    if(excelFile!==null){      
+    if(excelFile!==null){
       const workbook = XLSX.read(excelFile,{type:'buffer'});
       const worksheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[worksheetName];
+      const worksheet=workbook.Sheets[worksheetName];
       const data = XLSX.utils.sheet_to_json(worksheet);
       setExcelData(data);
     }
@@ -51,13 +50,11 @@ function App() {
       setExcelData(null);
     }
   }
-
-  // consoled
-  // console.log(excelData);
-
+  
   return (
     <div className="container">
 
+      {/* upload file section */}
       <div className='form'>
         <form className='form-group' autoComplete="off"
         onSubmit={handleSubmit}>
@@ -66,9 +63,7 @@ function App() {
           <input type='file' className='form-control'
           onChange={handleFile} required></input>                  
           {excelFileError&&<div className='text-danger'
-          style={{marginTop:5+'px'}}>
-            {excelFileError}
-          </div>}
+          style={{marginTop:5+'px'}}>{excelFileError}</div>}
           <button type='submit' className='btn btn-success'
           style={{marginTop:5+'px'}}>Submit</button>
         </form>
@@ -76,6 +71,8 @@ function App() {
 
       <br></br>
       <hr></hr>
+
+      {/* view file section */}
       <h5>View Excel file</h5>
       <div className='viewer'>
         {excelData===null&&<>No file selected</>}
